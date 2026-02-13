c4eabd01559b92edc48cd7bb78e25e00b1899845
AghaAmil <moravveji.amir.92@gmail.com>
2026-02-13 22:20:22 +0330

    add hangman game project

--- medium_challenges/1_hangman_game/hangman_game.py
+++ medium_challenges/1_hangman_game/hangman_game.py
@@ -1,3 +1,37 @@
 import random
 
+from artworks import HANGMAN_STAGES, LOGO
+from words_database import WORDS
 
+
+def game_start_message():
+    # """Display welcome banner and instructions to the user."""
+    print(f"{LOGO}\n")
+    print("=" * 50)
+    print(" Welcome to the Hangman Guessing Game")
+    print("=" * 50)
+
+
+def word_selection():
+    word = random.choice(WORDS)
+    return word
+
+
+def main():
+    game_start_message()
+
+    guessing_word = word_selection()
+    # testing purposes
+    print(guessing_word)
+
+    word_placeholder = ""
+    for letter in guessing_word:
+        word_placeholder += "- "
+
+    print(f"Word to Guess: {word_placeholder}")
+
+    user_guess = input("Guess a letter: ").strip().lower()
+
+
+if __name__ == "__main__":
+    main()
